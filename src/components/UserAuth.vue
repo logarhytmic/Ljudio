3c669921c9f12f2668ce7d491d93b3fd22f74a72
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-02-08 16:45:47 +0100

    User authentication interface frontend Added inputs for: forename, surname, email, password. Added two buttons, one is login and the other is register. Added simple styling and is using grid as display. Added two functions, at this point they're empty. But they're called, Login() and Register().

---
+++ src/components/UserAuth.vue
@@ -0,0 +1,50 @@
+<template>
+  <div id="container-auth">
+        <input v-model="_forename" type="text" id="input-forename" name="input-forename" alt="input-forename" placeholder="Forename...">
+        <input v-model="_surname" type="text" id="input-surname" name="input-surname" alt="input-surname" placeholder="Surname...">
+        <input v-model="_email" type="text" id="input-email" name="input-email" alt="input-email" placeholder="Email...">
+        <input v-model="_secret" type="password" id="input-secret" name="input-secret" alt="input-secret" placeholder="Password...">
+      <div id="btns-auth">
+        <button @click.prevent="Login()" id="btn-login">Login</button>
+        <button @click.prevent="Register()" id="btn-register">Register</button>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            _forename: "",
+            _surname: "",
+            _email: "",
+            _secret: ""
+        }
+    }, methods: {
+        Login() {
+        },
+        Register() {
+        }
+    }
+}
+</script>
+
+<style scoped>
+#container-auth {
+    display: grid;
+    grid-template-columns: auto;
+    grid-template-rows: auto;
+    justify-content: space-evenly;
+}
+
+input {
+    padding: 5px;
+    border: none;
+    border-bottom: 1px solid #ccc;
+}
+
+button {
+    margin-top: 8px;
+    margin-right: 2px;
+}
+</style>
