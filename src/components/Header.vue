df01910c5c7d7378e191d3d1486a4dae7261e0cf
jonathanlindahl <jonathanlindahl3@gmail.com>
2021-02-10 10:08:31 +0100

    grid structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,17 +1,29 @@
 <template>
     <header class="header">
         <h1>SPELA SHORELINE</h1>
+        <div class="user-auth">
+            <UserAuth />
+        </div>
     </header>
 </template>
 
 <script>
+import UserAuth from './UserAuth.vue'
+
 export default {
-    name: "Header"
+    name: 'Header',
+    components: {
+        UserAuth
+    },
 }
 </script>
 
 <style scoped>
     .header {
+        display: grid;
+        grid-template-columns: 50% 50%;
+        grid-template-rows: auto;
+        justify-content: space-evenly;
         position: fixed;
         top: 0px;
         width: 100%;
@@ -19,5 +31,9 @@ export default {
         color: #fff;
         text-align: center;
         padding: 20px;
+    }
+
+    .header > h1 {
+        align-self: center;
     }
 </style>
