85bb9631c6102b7a70360f95e4f4371095a61d0e
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-02-15 18:23:53 +0100

    Added a addSongToQueue function Added a removeSongFromQueue function  - !Warning; this function is suppose to return a response code whether it was successful or not. But mutations are void functions!

--- src/store.js
+++ src/store.js
@@ -1,12 +1,30 @@
 import { createStore } from 'vuex';
 
 const state = {
-    results: []
+    results: [],
+    queue: [],
 }
 
 const mutations = {
     addResults(state, results) {
         this.state.results = results;
+    },
+
+    addSongToQueue(state, song) {
+        this.state.queue.push(song);
+    },
+
+    removeSongFromQueue(state, song) {
+        // Mutations are void functions. They can not return anything. TODO: Convert this to an action with promise
+        let index = this.state.queue.findIndex((e) => Object.toJson(e) == Object.toJson(song));
+
+        if (index != -1) {
+            this.state.queue = this.state.queue.splice(index, 1);
+
+            return 1;
+        }
+
+        return 0;
     }
 }
 
