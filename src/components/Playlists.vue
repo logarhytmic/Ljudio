a8e6c60bcbb6e2d12c8e78548c2c21ad2523cbae
jonathanlindahl <jonathanlindahl3@gmail.com>
2021-02-15 10:41:38 +0100

    fetch playlists

--- src/components/Playlists.vue
+++ src/components/Playlists.vue
@@ -1,19 +1,57 @@
 <template>
     <div id="playlists">
         <p>playlists</p>
+        <span 
+            v-for="playlist in get_playlist"
+            :key="playlist.id"
+            class="playlist-item">
+                {{ playlist.title }}
+        </span>
     </div>
 </template>
 
 <script>
 export default {
+    // make a span for playlist items, v-for spans into the div, each span containing one playlist item
     data() {
-
+        return {
+            // TODO add number of songs
+            _data: {
+                id: "",
+                title: "",
+                user_id: "",
+            },
+        };
+    },
+    mounted() {
+        this.fetch_playlists("/api/playlists");
+        console.log(this.$store.state.playlists);
+    },
+    computed: {
+        get_playlist() {
+            return this.$store.state.playlists;
+        }
+    },
+    methods: {
+        async fetch_playlists(url) {
+            const res = await fetch(url, {
+                method: "GET",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+            });
+            await this.$store.commit("addPlaylists", res.json());
+        },
+        show_playlists() {
+            let playlist = this.fetch_playlists("/api/playlists");
+        }
     }
 }
 </script>
 
 <style scoped>
     .playlists {
-        display: grid;
+        display: flex;
+        flex-flow: column row;
     }
 </style>
--- src/store.js
+++ src/store.js
@@ -1,12 +1,16 @@
 import { createStore } from 'vuex';
 
 const state = {
-    results: []
+    results: [],
+    playlists: [],
 }
 
 const mutations = {
     addResults(state, results) {
         this.state.results = results;
+    },
+    addPlaylists(state, playlists) {
+        this.state.playlists = playlists;
     }
 }
 
