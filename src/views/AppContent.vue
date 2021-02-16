f11c1141b4d3259c46d7028d1b2210904c94f140
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-02-16 21:58:01 +0100

    Fixed some merge issues
    Moved code from my branch app.vue to AppContent.vue

---
+++ src/views/AppContent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div v-if="ifLoggedIn" id="container-main">
+    <div id="container-search">
+      <Search ref="search" v-on:play="onPlay" />
+    </div>
+    <div id="container-player">
+      <MusicPlayer ref="mPlayer" />
+    </div>
+  </div>
+</template>
+
+<script>
+import Search from "../components/Search.vue";
+import MusicPlayer from "../components/MusicPlayer.vue";
+import { mapGetters } from "vuex";
+
+export default {
+  name: "AppContent",
+  components: {
+    Search,
+    MusicPlayer,
+  },
+  methods: {
+    onPlay: function (song) {
+      this.$refs.mPlayer.playSong(song);
+    },
+  },
+  computed: {
+    ifLoggedIn() {
+      return this.$store.state.loggedIn;
+    },
+    ...mapGetters(["isLoggedIn"]),
+  },
+};
+</script>
+
+<style></style>
