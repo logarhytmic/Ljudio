71eb4f97a0f8518fdc39201e3e694e82bcb5ce58
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-02-16 21:54:57 +0100

    Changed name on Container variable to AppContent Changed file path for AppContent to the correct new views path Changed file path for Home to the correct new views path Changed the "/container" endpoint to "/app" for a better descriptive name
    Added aliases for the "default / root" endpoint
    
    !Warning; Tried to solve a bug that I found but was unable to. I will be making an issue of it!

--- src/router.js
+++ src/router.js
@@ -1,31 +1,42 @@
 import { createRouter, createWebHistory } from "vue-router";
 import store from './store.js'
-import Container from "./components/Container.vue";
-import Home from './components/Home.vue'
+import AppContent from "./views/AppContent.vue";
+import Home from './views/Home.vue'
 
 const routes = [
-    {
-        path: "/container",
-        name: "Container",
-        component: Container,
-        beforeEnter(to, from, next) {
-          if (store.getters.isLoggedIn) {
-            next();
-          } else {
-            next('/');
-          }
-        }
-    },
-    {
-        path: "/",
-        name: "Home",
-        component: Home
+  {
+    path: "/app",
+    name: "AppContent",
+    component: AppContent,
+    beforeEnter(to, from, next) {
+      if (store.getters.isLoggedIn) {
+        next();
+      } else {
+        next('/index');
+      }
     }
+  },
+  {
+    path: "/",
+    name: "Home",
+    component: Home,
+    alias: ["/index", "/home"],
+    // This isnt working.
+    // Expected result: If you're logged in and you try to enter the 3 endpoints; "/", "/home" or "/index" then you will be redirected to "/app"
+    // Actual result: It'll NOT take you to the "/app" endpoint. Instead it allows you to enter the root endpoint
+    // beforeEnter(to, from, next) {
+    //   if (store.getters.isLoggedIn) {
+    //     next('/app');
+    //   } else {
+    //     next();
+    //   }
+    // }
+  }
 ];
 
 const router = createRouter({
-    history: createWebHistory(),
-    routes,
-  });
-  
-  export default router;
+  history: createWebHistory(),
+  routes,
+});
+
+export default router;
