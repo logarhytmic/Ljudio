cf827c98dbbd736960bbfcc8863e9081477a5921
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-02-16 19:12:56 +0100

    Added a slider component Added a link between song duration and size of slider Added tooltip formatting to display seconds in HH:MM:SS Added a timer that checks elapsed time on video and changes slider value accordingly Added a toggle video icon. It allows you to show and hide video Added tooltips to all icons Added a h3 to display the current song above the video
    Changed the controls images to be em and google icons for easier styling
    Changed so that the player now goes hidden at mount by default

--- src/components/MusicPlayer.vue
+++ src/components/MusicPlayer.vue
@@ -1,44 +1,157 @@
 <template>
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
+
   <div class="mPlayer">
-    <div id="yt-player"></div>
-    <div class="bar">[progress bar here]<br /></div>
-    <div>
-      <img src="../assets/292-previous2.png" @click="previousVideo" />
-      <img src="../assets/285-play3.png" @click="playVideo" />
-      <img src="../assets/287-stop2.png" @click="pauseVideo" />
-      <img src="../assets/293-next2.png" @click="nextVideo" />
-      <img src="../assets/playlist_add.png" />
+    <h3>{{ fullname }}</h3>
+    <div id="show-player">
+      <div id="yt-player"></div>
+    </div>
+    <div class="bar">
+      <vue-slider
+        id="v-slider"
+        ref="slider"
+        v-model="value"
+        @drag-end="onDragEnd"
+        :min="0"
+        :max="max"
+        :contained="true"
+        :drag-on-click="true"
+        :tooltip="'active'"
+        :tooltip-formatter="durationFormatter"
+      ></vue-slider>
+      <div class="controls">
+        <em class="material-icons" title="Skip to previous video" @click="previousVideo">skip_previous</em>
+        <em class="material-icons" title="Play video" @click="playVideo">play_arrow</em>
+        <em class="material-icons" title="Pause video" @click="pauseVideo">pause</em>
+        <em class="material-icons" title="Skip to next video" @click="nextVideo">skip_next</em>
+        <em class="material-icons" title="Add current song to a playlist" @click="addToPlaylist">playlist_add</em>
+        <em class="material-icons" title="Show the video player" id="toggle-video" @click="onVideoToggle"
+          >tv</em
+        >
+        <br />
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+import VueSlider from "vue-slider-component";
+import "vue-slider-component/theme/material.css";
+
 export default {
   name: "MusicPlayer",
+  components: {
+    VueSlider,
+  },
   data() {
     return {
       player: null,
+      value: 0,
+      max: 1000,
+      durationFormatter: (e) => this.formatDuration(e * 1000),
+      videoTime: 0,
+      intervalUpdater: null,
+      fullname: "",
+      showVideo: 0,
     };
   },
 
   methods: {
+    onVideoToggle(event) {
+      if (this.showVideo === 1) {
+        event.target.style.color = "red";
+        this.showVideo = 0;
+        this.toggleVideoVisibility();
+      } else {
+        event.target.style.color = "green";
+        this.showVideo = 1;
+        this.toggleVideoVisibility();
+      }
+    },
+    toggleVideoVisibility() {
+      let playerDiv = document.getElementById("show-player");
+      let toggleEm = document.getElementById("toggle-video");
+
+      if (this.showVideo === 1) {
+        playerDiv.style.visibility = "visible";
+        toggleEm.title = "Hide the video player";
+      } else {
+        playerDiv.style.visibility = "hidden";
+        toggleEm.title = "Show the video player";
+      }
+    },
+    formatDuration(ms) {
+      let s = (ms / 1000).toFixed(0);
+      let m = Math.floor(s / 60);
+      let h = "";
+
+      if (m > 59) {
+        h = Math.floor(m / 60);
+        h = h >= 10 ? h : "0" + h;
+        m = m - h * 60;
+        m = m >= 10 ? m : "0" + m;
+      }
+
+      s = Math.floor(s % 60);
+      s = s >= 10 ? s : "0" + s;
+
+      return h != "" ? h + ":" + m + ":" + s : m + ":" + s;
+    },
     playVideo() {
       this.player.playVideo();
     },
     pauseVideo() {
       this.player.pauseVideo();
     },
-    nextsVideo() {
-      this.player.nextideo();
+    nextVideo() {
+      this.player.nextVideo();
     },
     previousVideo() {
       this.player.previousVideo();
+    },
+    onPlayerReady(event) {},
+    onPlayerStateChange(event) {
+      if (event.data != YT.PlayerState.PLAYING) {
+        this.intervalUpdater = setInterval(this.updateTime, 1000);
+      } else if (
+        event.data != YT.PlayerState.PAUSED ||
+        event.data != YT.PlayerState.ENDED
+      ) {
+        clearInterval(this.intervalUpdater);
+      }
+    },
+    updateTime() {
+      let oldTime = this.videoTime;
+      if (this.player && this.player.getCurrentTime()) {
+        this.videoTime = this.player.getCurrentTime();
+      }
+
+      if (this.videoTime !== oldTime) {
+        this.onProgress(this.videoTime);
+      }
+    },
+    onProgress(currentTime) {
+      if (currentTime > 1) {
+        this.value = currentTime;
+      }
+    },
+    playSong(song) {
+      this.player.loadVideoById(song.Id);
+      this.value = 0;
+      this.max = song.Duration / 1000;
+      this.fullname = song.Originator + " - " + song.Name;
+    },
+    onDragEnd(event) {
+      this.player.seekTo(this.$refs.slider.getValue(), true);
     },
   },
 
   async mounted() {
     this.player = new YT.Player("yt-player", {
-      videoId: "ZeSN4PntOos",
+      videoId: "",
       host: "https://www.youtube.com",
       playerVars: {
         autoplay: 0,
@@ -47,18 +160,15 @@ export default {
       height: "300",
       width: "400",
       events: {
-        //'onStateChange': onPlayerStateChange
+        onReady: this.onPlayerReady,
+        onStateChange: this.onPlayerStateChange,
       },
     });
+
+    document.getElementById("show-player").style.visibility = "hidden";
   },
 
-  computed: {
-    playSong(id) {
-      this.player.loadVideoById(id);
-    },
-  },
-
-  props: {},
+  computed: {},
 };
 </script>
 
@@ -70,10 +180,22 @@ export default {
     "v v v v v"
     "b b b b b"
     "c c c c c";
+  padding-top: 47px;
 }
 
-.mPlayer img:active {
+.mPlayer > img:active {
   transform: scale(0.9);
+}
+
+.mPlayer > h3 {
+  color: white;
+}
+
+#v-slider {
+  margin-top: 5px;
+  margin-right: 10px;
+  margin-bottom: 5px;
+  margin-left: 10px;
 }
 
 .yt-player {
@@ -82,5 +204,29 @@ export default {
 
 .bar {
   grid-area: b;
+  width: 60%;
+  margin: 0 auto;
+  border: 1px solid #231123;
+  box-shadow: 5px 5px 5px black;
+}
+
+.controls {
+  color: white;
+}
+
+.controls > em {
+  cursor: pointer;
+}
+
+.controls > em:hover {
+  color: coral;
+}
+
+.controls > em:active {
+  color: white;
+}
+
+#toggle-video {
+  color: red;
 }
 </style>
