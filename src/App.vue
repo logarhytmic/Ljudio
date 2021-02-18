c8007aa019e13b7802064edcbfcb0a2217143851
Christoffer Hansen <chris.hansen.ch@outlook.com>
2021-02-19 00:29:22 +0100

    Moved styling imports from other components to this

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,12 @@
 <template>
+<link
+    rel="stylesheet"
+    href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"
+  />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons"
+  />
   <router-view />
 </template>
 
